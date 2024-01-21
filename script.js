c0f9aafe1dc98f9b328c337ffc4eabd8aea7c127
Madiyar-1 <157319992+Madiyar-1@users.noreply.github.com>
2024-01-21 23:05:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function uploadPhoto() {
     const fileInput = document.getElementById('fileInput');
     const gallery = document.getElementById('gallery');
+    const category = document.getElementById('category').value;
 
     const file = fileInput.files[0];
 
@@ -11,25 +12,11 @@ function uploadPhoto() {
             const img = document.createElement('img');
             img.src = e.target.result;
             img.className = 'gallery-img';
-            img.dataset.topic = 'user-uploaded';
+            img.dataset.topic = category;
             gallery.appendChild(img);
         };
 
         reader.readAsDataURL(file);
-    }
-}
-
-function filterByTopic() {
-    const topicSelector = document.getElementById('topicSelector');
-    const selectedTopic = topicSelector.value;
-    const galleryImages = document.getElementsByClassName('gallery-img');
-
-    for (const img of galleryImages) {
-        if (selectedTopic === 'all' || img.dataset.topic === selectedTopic) {
-            img.style.display = 'inline-block';
-        } else {
-            img.style.display = 'none';
-        }
     }
 }
 
